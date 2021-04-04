0fb4827b15bb674f077b1ca11520393f8a83ec41
JJJoonngg <whdtls3878@gmail.com>
2021-04-04 23:12:19 +0900

    3.Use ConstraintLayout with the Layout Editor - 11~12

--- Unit1/3.Layouts/3.Use ConstraintLayout with the Layout Editor/ColorMyViews/app/src/main/java/kr/co/jjjoonngg/colormyviews/MainActivity.kt
+++ Unit1/3.Layouts/3.Use ConstraintLayout with the Layout Editor/ColorMyViews/app/src/main/java/kr/co/jjjoonngg/colormyviews/MainActivity.kt
@@ -4,9 +4,15 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var boxThreeText: TextView
+    lateinit var boxFourText: TextView
+    lateinit var boxFiveText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,21 +24,25 @@ class MainActivity : AppCompatActivity() {
 
         val boxOneText = findViewById<TextView>(R.id.box_one_text)
         val boxTwoText = findViewById<TextView>(R.id.box_two_text)
-        val boxThreeText = findViewById<TextView>(R.id.box_three_text)
-        val boxFourText = findViewById<TextView>(R.id.box_four_text)
-        val boxFiveText = findViewById<TextView>(R.id.box_five_text)
+        boxThreeText = findViewById(R.id.box_three_text)
+        boxFourText = findViewById(R.id.box_four_text)
+        boxFiveText = findViewById(R.id.box_five_text)
 
         val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
 
+        val redButton = findViewById<Button>(R.id.red_button)
+        val greenButton = findViewById<Button>(R.id.green_button)
+        val yellowButton = findViewById<Button>(R.id.yellow_button)
+
         val clickableViews: List<View> = listOf(
             boxOneText, boxTwoText, boxThreeText,
-            boxFourText, boxFiveText, rootConstraintLayout
+            boxFourText, boxFiveText, rootConstraintLayout,
+            redButton, greenButton, yellowButton
         )
 
         for (item in clickableViews) {
             item.setOnClickListener {
                 makeColored(it)
-                setBackground(it)
             }
         }
     }
@@ -44,17 +54,10 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.red_button -> boxThreeText.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> boxFourText.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> boxFiveText.setBackgroundResource(R.color.my_green)
             else -> view.setBackgroundColor(Color.LTGRAY)
-        }
-    }
-
-    private fun setBackground(view: View) {
-        when (view.id) {
-            R.id.box_one_text -> view.setBackgroundResource(R.drawable.image_one)
-            R.id.box_two_text -> view.setBackgroundResource(R.drawable.image_two)
-            R.id.box_three_text -> view.setBackgroundResource(R.drawable.image_three)
-            R.id.box_four_text -> view.setBackgroundResource(R.drawable.image_four)
-            R.id.box_five_text -> view.setBackgroundResource(R.drawable.image_five)
         }
     }
 }
