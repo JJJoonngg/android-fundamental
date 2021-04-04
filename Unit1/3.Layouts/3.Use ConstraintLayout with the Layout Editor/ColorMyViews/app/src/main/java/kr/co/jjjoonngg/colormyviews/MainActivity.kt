17d170dcaf0dee2806cd083f3051b0c27097db76
JJJoonngg <whdtls3878@gmail.com>
2021-04-04 22:28:58 +0900

    3.Use ConstraintLayout with the Layout Editor - 1~8

---
+++ Unit1/3.Layouts/3.Use ConstraintLayout with the Layout Editor/ColorMyViews/app/src/main/java/kr/co/jjjoonngg/colormyviews/MainActivity.kt
@@ -0,0 +1,47 @@
+package kr.co.jjjoonngg.colormyviews
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setListeners()
+    }
+
+    private fun setListeners() {
+
+        val boxOneText = findViewById<TextView>(R.id.box_one_text)
+        val boxTwoText = findViewById<TextView>(R.id.box_two_text)
+        val boxThreeText = findViewById<TextView>(R.id.box_three_text)
+        val boxFourText = findViewById<TextView>(R.id.box_four_text)
+        val boxFiveText = findViewById<TextView>(R.id.box_five_text)
+
+        val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
+
+        val clickableViews: List<View> = listOf(
+            boxOneText, boxTwoText, boxThreeText,
+            boxFourText, boxFiveText, rootConstraintLayout
+        )
+
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColored(it) }
+        }
+    }
+
+    private fun makeColored(view: View) {
+        when (view.id) {
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
+            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
+    }
+}
